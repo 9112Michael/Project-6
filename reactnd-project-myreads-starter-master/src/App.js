a4dc65c193bf5e3e899bc70fd74e8313cf756455
9112Michael <sitkabikemike@yahoo.com>
2018-09-16 12:31:41 -0700

    Searchpage component and back button function

--- reactnd-project-myreads-starter-master/src/App.js
+++ reactnd-project-myreads-starter-master/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
+import SearchPage from './Components/Searchpage'
 
 class BooksApp extends React.Component {
   state = {
@@ -13,32 +14,15 @@ class BooksApp extends React.Component {
     showSearchPage: false
   }
 
+  backClick =() => {
+    this.setState({ showSearchPage: false })
+  }
+
   render() {
     return (
       <div className="app">
         {this.state.showSearchPage ? (
-          //Beginning Search page
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-          //End Search page
+         <SearchPage backClick={this.backClick} />
         ) : (
           //Start Main page
           <div className="list-books">
