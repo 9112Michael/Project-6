8b5699856cac18565e9b7548d5f8f30983df0b3d
9112Michael <sitkabikemike@yahoo.com>
2018-09-17 07:05:51 -0700

    Created Books component

---
+++ reactnd-project-myreads-starter-master/src/Components/Books.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+
+export default class Books extends Component {
+  render() {
+
+    const { book }= this.props;
+    const { imageLinks, title, authors, shelf }=book;
+    console.log(book);
+    return (
+
+       <li>
+       <div className="book">
+         <div className="book-top">
+           <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${imageLinks.thumbnail})` }}></div>
+           <div className="book-shelf-changer">
+             <select value={shelf} onChange={() => {}}>
+               <option value="move" disabled>Move to...</option>
+               <option value="currentlyReading">Currently Reading</option>
+               <option value="wantToRead">Want to Read</option>
+               <option value="read">Read</option>
+               <option value="none">None</option>
+             </select>
+           </div>
+         </div>
+         <div className="book-title">{title}</div>
+         {authors.map(author => (<div key= {author} className="book-authors">{author}</div>))}
+       </div>
+     </li>
+    )
+  }
+}
