37b8ab54eb47b31883b5f0b728f4f7af47aa76db
9112Michael <sitkabikemike@yahoo.com>
2018-09-18 18:53:23 -0700

    State management, using API method, getAll() and componentDidMount

--- reactnd-project-myreads-starter-master/src/App.js
+++ reactnd-project-myreads-starter-master/src/App.js
@@ -1,19 +1,24 @@
 import React from 'react'
-//import * as BooksAPI from './Data/BooksAPI'
+import * as BooksAPI from './Data/BooksAPI'
 import './App.css'
 import SearchPage from './Components/Searchpage'
 import MainPage from './Components/Mainpage'
-import {APIBooks} from './Data/Bookdata'
+//import {APIBooks} from './Data/Bookdata'
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
+constructor(props) {
+  super(props)
+
+  this.state = {
+    books: [],
     showSearchPage: false
+   }
+  }
+
+  componentDidMount = () => {
+    BooksAPI.getAll()
+    .then(res => this.setState({books: res}))
+    .catch(err => { });
   }
 
   backClick =() => {
@@ -25,9 +30,7 @@ class BooksApp extends React.Component {
   }
 
   render() {
-
-    const books = APIBooks;
-    
+    const {books} = this.state;
 
     return (
       <div className="app">
